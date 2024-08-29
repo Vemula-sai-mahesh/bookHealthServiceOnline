ef3a5740bc699b8ead1142cfac8ba67807784575
VEMULA SAI MAHESH <125812065+Vemula-sai-mahesh@users.noreply.github.com>
2024-08-29 16:35:50 +0530

    cors filter is added.

--- src/main/java/com/example/BookHealthServiceOnline/config/WebConfig.java
+++ src/main/java/com/example/BookHealthServiceOnline/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500","http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:5500","http://127.0.0.1:5500","http://localhost:8006","http://192.168.2.113:8006","http://192.168.2.113:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
